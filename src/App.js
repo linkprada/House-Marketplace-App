95c4ae353c1c42c33c23a4d5ef0f1be70e789fdc
linkprada <haouaouchi.said@gmail.com>
2022-02-02 20:26:32 +0100

    Add Some Routes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Explore from "./pages/Explore";
+import Offers from "./pages/Offers";
+import Profile from "./pages/Profile";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import ForgotPassword from "./pages/ForgotPassword";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <Router>
+            <Routes>
+                <Route path="/" element={<Explore></Explore>}></Route>
+                <Route path="/offers" element={<Offers></Offers>}></Route>
+                <Route path="/profile" element={<Profile></Profile>}></Route>
+                <Route path="/sign-in" element={<SignIn></SignIn>}></Route>
+                <Route path="/sign-up" element={<SignUp></SignUp>}></Route>
+                <Route path="/forgotpassword" element={<ForgotPassword></ForgotPassword>}></Route>
+            </Routes>
+        </Router>
+    );
 }
 
 export default App;
