f7c4f2971920dfeb0895563f81564d8a5313d2af
linkprada <haouaouchi.said@gmail.com>
2022-02-03 22:35:22 +0100

    Add React-Toastify and Use Notification in SignUp/SignIn

--- src/App.js
+++ src/App.js
@@ -6,20 +6,28 @@ import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
 import ForgotPassword from "./pages/ForgotPassword";
 import Navbar from "./components/Navbar";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function App() {
     return (
-        <Router>
-            <Routes>
-                <Route path="/" element={<Explore></Explore>}></Route>
-                <Route path="/offers" element={<Offers></Offers>}></Route>
-                <Route path="/profile" element={<Profile></Profile>}></Route>
-                <Route path="/sign-in" element={<SignIn></SignIn>}></Route>
-                <Route path="/sign-up" element={<SignUp></SignUp>}></Route>
-                <Route path="/forgot-password" element={<ForgotPassword></ForgotPassword>}></Route>
-            </Routes>
-            <Navbar></Navbar>
-        </Router>
+        <>
+            <Router>
+                <Routes>
+                    <Route path="/" element={<Explore></Explore>}></Route>
+                    <Route path="/offers" element={<Offers></Offers>}></Route>
+                    <Route path="/profile" element={<Profile></Profile>}></Route>
+                    <Route path="/sign-in" element={<SignIn></SignIn>}></Route>
+                    <Route path="/sign-up" element={<SignUp></SignUp>}></Route>
+                    <Route
+                        path="/forgot-password"
+                        element={<ForgotPassword></ForgotPassword>}
+                    ></Route>
+                </Routes>
+                <Navbar></Navbar>
+            </Router>
+            <ToastContainer></ToastContainer>
+        </>
     );
 }
 
--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -5,6 +5,7 @@ import visibilityIcon from "../assets/svg/visibilityIcon.svg";
 import { getAuth, createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
 import { doc, setDoc, serverTimestamp } from "firebase/firestore";
 import { DataBase } from "../firebase.config";
+import { toast } from "react-toastify";
 
 function SignUp() {
     const [showPassword, setShowPassword] = useState(false);
@@ -46,7 +47,7 @@ function SignUp() {
 
             navigate("/");
         } catch (error) {
-            console.log(error);
+            toast.error("Something went wrong with registration");
         }
     };
 
