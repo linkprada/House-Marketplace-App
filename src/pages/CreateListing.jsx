c6a6f75b06e6ab974d2bb091526f1014c9f9c8e8
linkprada <haouaouchi.said@gmail.com>
2022-02-11 18:59:51 +0100

    Handle Submit Data in CreateListing

--- src/pages/CreateListing.jsx
+++ src/pages/CreateListing.jsx
@@ -1,11 +1,16 @@
 import { getAuth, onAuthStateChanged } from "firebase/auth";
 import { useEffect, useRef, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
 import Spinner from "../components/Spinner";
+import { getStorage, ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
+import { v4 as uuidv4 } from "uuid";
+import { DataBase } from "../firebase.config";
+import { addDoc, collection, serverTimestamp } from "firebase/firestore";
 
 function CreateListing() {
     const [loading, setLoading] = useState(false);
-    const [geolocationEnabled, setGeolocationEnabled] = useState(true);
+    const [geolocationEnabled, setGeolocationEnabled] = useState(false);
     const [formData, setFormData] = useState({
         type: "rent",
         name: "",
@@ -62,6 +67,109 @@ function CreateListing() {
 
     const onSubmit = async (e) => {
         e.preventDefault();
+
+        setLoading(true);
+
+        if (discountedPrice >= regularPrice) {
+            setLoading(false);
+            toast.error("Discounted price needs to be less than regular price");
+            return;
+        }
+
+        if (images.length > 6) {
+            setLoading(false);
+            toast.error("Max 6 images");
+            return;
+        }
+
+        let geolocation = {};
+        let location;
+
+        if (geolocationEnabled) {
+            const response = await fetch(
+                `https://maps.googleapis.com/maps/api/geocode/json?address=${address}&key=${process.env.REACT_APP_GEOCODE_API_KEY}`
+            );
+
+            const data = await response.json();
+            console.log(data);
+
+            geolocation.lat = data.results[0]?.geometry.location.lat ?? 0;
+            geolocation.lng = data.results[0]?.geometry.location.lng ?? 0;
+
+            location =
+                data.status === "ZERO_RESULTS" ? undefined : data.results[0]?.formatted_address;
+
+            if (location === undefined || location.includes("undefined")) {
+                setLoading(false);
+                toast.error("Please enter a correct address");
+                return;
+            }
+        } else {
+            geolocation.lat = latitude;
+            geolocation.lng = longitude;
+        }
+
+        const storeImage = async (image) => {
+            return new Promise((resolve, reject) => {
+                const storage = getStorage();
+                const fileName = `${auth.currentUser.uid}-${image.name}-${uuidv4()}`;
+
+                const storageRef = ref(storage, "images/" + fileName);
+
+                const uploadTask = uploadBytesResumable(storageRef, image);
+
+                uploadTask.on(
+                    "state_changed",
+                    (snapshot) => {
+                        const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+                        console.log("Upload is " + progress + "% done");
+                        switch (snapshot.state) {
+                            case "paused":
+                                console.log("Upload is paused");
+                                break;
+                            case "running":
+                                console.log("Upload is running");
+                                break;
+                            default:
+                                break;
+                        }
+                    },
+                    (error) => {
+                        reject(error);
+                    },
+                    () => {
+                        getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+                            resolve(downloadURL);
+                        });
+                    }
+                );
+            });
+        };
+
+        const imageUrls = await Promise.all([...images].map((image) => storeImage(image))).catch(
+            () => {
+                setLoading(false);
+                toast.error("Images not uploaded");
+                return;
+            }
+        );
+
+        const formDataCopy = {
+            ...formData,
+            imageUrls,
+            geolocation,
+            timestamp: serverTimestamp(),
+        };
+
+        formDataCopy.location = address;
+        delete formDataCopy.images;
+        delete formDataCopy.address;
+        !formDataCopy.offer && delete formDataCopy.discountedPrice;
+
+        const docRef = await addDoc(collection(DataBase, "listings"), formDataCopy);
+        setLoading(false);
+        toast.success("Listing saved");
+        navigate(`/category/${formDataCopy.type}/${docRef.id}`);
     };
 
     const onMutate = (e) => {
