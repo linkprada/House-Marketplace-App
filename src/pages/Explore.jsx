e1306712884a1b9c486b49f2b9bd0da5634312b4
linkprada <haouaouchi.said@gmail.com>
2022-02-14 19:27:07 +0100

    Add Slider Component and Use In Explore Page

--- src/pages/Explore.jsx
+++ src/pages/Explore.jsx
@@ -1,6 +1,7 @@
 import { Link } from "react-router-dom";
 import rentCategoryImage from "../assets/jpg/rentCategoryImage.jpg";
 import sellCategoryImage from "../assets/jpg/sellCategoryImage.jpg";
+import Slider from "../components/Slider";
 
 function Explore() {
     return (
@@ -8,6 +9,8 @@ function Explore() {
             <header>
                 <p className="pageHeader">Explore</p>
             </header>
+
+            <Slider></Slider>
 
             <main>
                 <p className="exploreCategoryHeading">Categories</p>
